9ff6ba9b09b391bd2b441ca4fd932a4d5cc0a02d
liuhaonan <acerola.orion@foxmail.com>
2020-05-20 15:49:31 +0800

    fixed(#user): fix bug of login param validation

--- easyshop-commons-entity/src/main/java/com/github/mayoi7/easyshop/dto/user/LoginParam.java
+++ easyshop-commons-entity/src/main/java/com/github/mayoi7/easyshop/dto/user/LoginParam.java
@@ -15,11 +15,11 @@ public class LoginParam implements Serializable {
 
     private static final long serialVersionUID = -1144854814680952811L;
 
-    /** 用户名，6-20位 */
-    @Pattern(regexp = "^[^0-9][\\\\w_]{5,19}$", message = "用户名格式错误，应为6-20位的合法字符")
+    /** 用户名，5-20位 */
+    @Pattern(regexp = "^[^0-9][\\w_]{4,19}$", message = "用户名格式错误，应为5-20位的合法字符")
     private String username;
 
-    /** 密码，6-20位 */
-    @Pattern(regexp = "^[\\\\w_]{6,20}$", message = "密码格式错误，应为6-20位的合法字符")
+    /** 密码，5-20位 */
+    @Pattern(regexp = "^[\\w_]{5,20}$", message = "密码格式错误，应为5-20位的合法字符")
     private String password;
 }
