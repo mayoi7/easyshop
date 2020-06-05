5e7519ca29e20ff2f7d0c05962cae2fa5a325640
liuhaonan <acerola.orion@foxmail.com>
2020-06-05 10:29:37 +0800

    feat(#frontend): add total amount statistic data stage

--- easyshop-frontend/src/store/index.js
+++ easyshop-frontend/src/store/index.js
@@ -5,6 +5,7 @@ import app from './modules/app'
 import settings from './modules/settings'
 import user from './modules/user'
 import shop from "./modules/shop";
+import count from "./modules/count";
 
 Vue.use(Vuex)
 
@@ -13,7 +14,8 @@ const store = new Vuex.Store({
     app,
     settings,
     user,
-    shop
+    shop,
+    count
   },
   getters
 })
